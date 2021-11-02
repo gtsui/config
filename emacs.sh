0b19e7db80aa17ef7e6f2a4208b3ea48c96692f1
gtsui <tsui.geoffrey@gmail.com>
2021-11-02 16:53:23 +0800

    separate emacs.sh

---
+++ emacs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#Copy config file
+cp .emacs ~
+
+#Install emacs
+cd ~
+sudo apt-get install emacs
+echo "alias emacs='emacs -nw'" >> .bashrc
+echo 'export PATH="$PATH:/home/tsuigeo/tsuigeo/bin"' >> .bashrc
+
+#Setup emacs library
+mkdir ~/tsuigeo/lib/emacs
--- setup.sh
+++ setup.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-cp .emacs ~
-
 cd ~
 mkdir tsuigeo
 cd tsuigeo
@@ -11,13 +9,3 @@ mkdir src
 mkdir data
 mkdir etc
 mkdir tmp && mkdir tmp/downloads
-
-#install emacs
-cd ~
-sudo apt-get install emacs
-echo "alias emacs='emacs -nw'" >> .bashrc
-echo 'export PATH="$PATH:/home/tsuigeo/tsuigeo/bin"' >> .bashrc
-
-#install ssh server
-sudo apt-get install openssh-server ii
-
